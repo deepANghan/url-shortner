907b60309ba025c7ea0b911ed613bf13f56dcd15
d_star <deepanghan03@gmail.com>
2026-04-29 11:45:10 +0530

    some alignment done

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,9 +13,7 @@ const PORT = configs.PORT;
 app.use(cors({
     origin: ["http://127.0.0.1:5500"]
 }));
-
 app.use(express.json());
-
 app.use(requestLogger);
 
 app.use("/api/v1/url", urlRouter);
@@ -28,7 +26,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     })
 
     return;
-
 });
 
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
