f8b988fae46317b9ad1d2a57a7e003411d89cc6f
d_star <deepanghan03@gmail.com>
2026-04-29 12:04:18 +0530

    line changes

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const configs = {
-    NODE_ENV:process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
 }
 
