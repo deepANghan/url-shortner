295cd01394051b143631d5ab0799bc41360888dd
d_star <deepanghan03@gmail.com>
2026-05-02 19:33:40 +0530

    docker-compose for redis and rabitmq

--- server/src/routes/urls.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getURL, postURL } from "../controllers/urls.controllers.js";
-
-const urlRouter = Router();
-
-urlRouter.get("/:shortUrl", getURL);
-urlRouter.post("/", postURL);
-
-export { urlRouter };
--- server/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-    test: {
-        // ... Specify options here.
-        globals: true,
-        environment: "node"
-    },
-})
